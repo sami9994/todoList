3f777477b7fa1763a1b15adcaf4a8eb126c0b78e
sami9994 <samiomar994@gmail.com>
2022-12-08 16:48:23 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import './header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <h1>TODO LIST</h1>
+    </div>
+  )
+}
+export default Header
---
+++ src/components/TodoList.js
@@ -0,0 +1,5 @@
+import './todolist.css'
+const TodoList = () => {
+  return <div className='todo-list-body'>TodoList</div>
+}
+export default TodoList
